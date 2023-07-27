ab5b7be60c46e3a3e16b1b6aca41569f92969eb5
Raymundo12 <raymundo.olverap@gmail.com>
2023-07-27 13:45:35 +0100

    Homepage

--- Pages/01_Home.py
+++ Pages/01_Home.py
@@ -2,20 +2,42 @@ import pickle
 import streamlit as st
 import streamlit_authenticator as stauth
 from pathlib import Path
+from streamlit_option_menu import option_menu
 
 st.set_page_config(page_title="01_Home", layout="wide")
 
+with st.sidebar:
+    selected = option_menu("Sales Menu", ["Home", 'Store Sales', 'Area Sales', 'Category Sales', 'Sales by Supplier'],
+    icons=['house-fill', 'shop','globe-europe-africa', 'diagram-3', 'cart3'], menu_icon="bar-chart-line-fill", default_index=1,
+
+    styles={
+        "container": {"padding": "0!important", "background-color": "#fafafa"},
+        "icon": {"color": "orange", "font-size": "25px"}, 
+        "nav-link": {"font-size": "20px", "text-align": "left", "margin":"0px", "--hover-color": "#eee"},
+        "nav-link-selected": {"background-color": "green"},
+    })
+
 st.title ('Welcome to HS Sales HUB')
 
-names = ['John Smith','Rebecca Briggs']
-usernames = ['jsmith','rbriggs']
-passwords = ['123','456']
+# 4. Manual Item Selection
+if st.session_state.get('switch_button', False):
+    st.session_state['menu_option'] = (st.session_state.get('menu_option',0) + 1) % 4
+    manual_select = st.session_state['menu_option']
+else:
+    manual_select = None
+    
+selected4 = option_menu(None, ["Home", 'Store Sales', 'Area Sales', 'Category Sales', 'Sales by Supplier'], 
+    icons=['house-fill', 'shop','globe-europe-africa', 'diagram-3', 'cart3'], 
+    orientation="horizontal", manual_select=manual_select, key='menu_4')
+st.button(f"Move to Next {st.session_state.get('menu_option',1)}", key='switch_button')
+selected4
 
-hashed_passwords = stauth.Hasher(passwords).generate()
-
-
-authenticator = stauth.authenticate(names,usernames,hashed_passwords, 
-    'some_cookie_name','some_signature_key',cookie_expiry_days=30)
-
-name, authentication_status = authenticator.login('Login','main')
-
+# 5. Add on_change callback
+def on_change(key):
+    selection = st.session_state[key]
+    st.write(f"Selection changed to {selection}")
+    
+selected5 = option_menu(None, ["Home", 'Store Sales', 'Area Sales', 'Category Sales', 'Sales by Supplier'],
+                        icons=['house-fill', 'shop','globe-europe-africa', 'diagram-3', 'cart3'],
+                        on_change=on_change, key='menu_5', orientation="horizontal")
+selected5
