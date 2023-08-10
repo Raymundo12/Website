2457f789bf55f768b65eb62a6d8ddb5c439d4044
Raymundo12 <61193663+Raymundo12@users.noreply.github.com>
2023-08-10 13:45:45 +0100

    home1

--- Pages/01_Home.py
+++ Pages/01_Home.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 def app():
 
-st.title('Welcome to HS Sales HUB')
+ st.title('Welcome to HS Sales HUB')
 
 email=st.text_input('Email Address')
 password=st.text_input('Password', type='password')
