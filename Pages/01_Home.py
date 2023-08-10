7a66ae2d1b5160e616d85c224eb35fb750535126
Raymundo12 <61193663+Raymundo12@users.noreply.github.com>
2023-08-10 13:41:30 +0100

    New logginform

--- Pages/01_Home.py
+++ Pages/01_Home.py
@@ -1,14 +1,8 @@
-import json
-import pickle
 import streamlit as st
-import streamlit_authenticator as stauth
-from pathlib import Path
-from streamlit_option_menu import option_menu
-from streamlit.source_util import _on_pages_changed, get_pages
 
-DEFAULT_PAGE ="01_Home.py"
 
-##def get_all_pages():
-    
 
-st.set_page_config(page_title="01_Home", layout="wide")
+email=st.text_input('Email Address')
+password=st.text_input('Password', type='password')
+
+st.button('Login')
