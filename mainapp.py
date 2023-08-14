7e9f47d0cf947745827918c2c017046e71f6df48
Raymundo12 <61193663+Raymundo12@users.noreply.github.com>
2023-08-14 15:03:43 +0100

    mainapp

--- mainapp.py
+++ mainapp.py
@@ -12,3 +12,22 @@ st.set_page_config(page_title="HS Sales HUB 2023",
 
 st.title('Welcome to :green[HS Sales HUB]')
 
+df = pd.read_excel("dfstoreTotal.xlsx")
+
+st.markdown("##")
+
+st.sidebar.header('Please select the Week:')
+
+Week = st.sidebar.multiselect(
+    "Select the Week:",
+    options=df["Week"].unique())
+
+Store = st.sidebar.selectbox(
+ "Select the Store:",
+  options=df["Stores"].unique())
+
+Store2 = st.sidebar.selectbox(
+ "Select the Store to Compare:",
+  options=df["Stores"].unique())
+
+st.markdown("##")
