be9f48601060c870938e69d957818472ad256693
Raymundo12 <61193663+Raymundo12@users.noreply.github.com>
2023-08-14 15:05:52 +0100

    comit

--- mainapp.py
+++ mainapp.py
@@ -1,11 +1,5 @@
-import pickle
-from pathlib import Path
-import streamlit_authenticator as stauth
 import streamlit as st
 import pandas as pd
-import numpy as np
-import plotly.express as px
-import openpyxl
 
 st.set_page_config(page_title="HS Sales HUB 2023",
                    page_icon="bar_chart:",
